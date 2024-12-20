8854aac3afc533deb1cfdfb30466af57d2a1033a
LairdLuca <luca.sanna.1503@gmail.com>
2024-12-20 01:44:55 +0100

    Understanding Unit Testing .NET Core Applications

---
+++ src/Test-Driven-Development/RoomBookingApp.API.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using RoomBookingApp.API.Controllers;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomBookingApp.API.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Should_Return_Forcast_Results()
+        {
+            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(loggerMock.Object);
+
+            var result = controller.Get();
+
+            result.Count().ShouldBeGreaterThan(1);
+            result.ShouldNotBeNull();
+        }
+    }
+}
---
+++ src/Test-Driven-Development/RoomBookingApp.API/WeatherForecast.cs
@@ -0,0 +1,10 @@
+﻿
+namespace RoomBookingApp.API
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public string Summary { get; set; }
+    }
+}
